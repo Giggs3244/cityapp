6aee11a411e508977573717ba321a78bb16d8167
bryan.bedoya <bryan.bedoya@bryanbedoya.pragma.com.co>
2017-02-09 17:27:09 -0500

    Add Resources

--- cityapp.webservices/src/main/java/cityapp/webservices/CountryResource.java
+++ cityapp.webservices/src/main/java/cityapp/webservices/CountryResource.java
@@ -1,5 +1,17 @@
 package cityapp.webservices;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("countries")
 public class CountryResource {
 
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getSaludo() {
+		return "{\"Nombre\":\"Bryan\", \"Apellido\":\"Bedoya\"}";
+	}
+	
 }
